f8781f45dcec499d5a181c05c0bea50fdea9b81a
Monika <monikadkale98@gmail.com>
2021-03-12 08:43:04 +0530

    added cheap and best rate

--- Hotel.java
+++ Hotel.java
@@ -4,18 +4,15 @@ public class Hotel {
 	
 	private String name;
     private int rate;
+    private int weekdayrate;
+    private int weekendrate;
+    
+	
 
-    public Hotel(String name, int rate) {
-        this.setName(name);
-        this.setRate(rate);
-    }
-
-	public int getRate() {
-		return rate;
-	}
-
-	public void setRate(int rate) {
-		this.rate = rate;
+	@Override
+	public String toString() {
+		return "Hotel [name=" + name + ", rate=" + rate + ", weekdayrate=" + weekdayrate + ", weekendrate="
+				+ weekendrate + "]";
 	}
 
 	public String getName() {
@@ -25,4 +22,37 @@ public class Hotel {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public int getWeekdayrate() {
+		return weekdayrate;
+	}
+
+	public void setWeekdayrate(int weekdayrate) {
+		this.weekdayrate = weekdayrate;
+	}
+
+	public int getWeekendrate() {
+		return weekendrate;
+	}
+
+	public void setWeekendrate(int weekendrate) {
+		this.weekendrate = weekendrate;
+	}
+
+	public Hotel(String name, int rate, int weekdayrate, int weekendrate) {
+		super();
+		this.name = name;
+		this.rate = rate;
+		this.weekdayrate = weekdayrate;
+		this.weekendrate = weekendrate;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+   
 }
--- HotelReservationService.java
+++ HotelReservationService.java
@@ -1,12 +1,19 @@
 package com.hotel;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+import com.google.common.base.Predicate;
 
 public class HotelReservationServise {
 	
 	
-	private List<Hotel> hotelList = new ArrayList<>();
+	List<Hotel> hotelList = new ArrayList<>();
 
     public void addHotel(Hotel hotel) {
         hotelList.add(hotel);
@@ -14,5 +21,18 @@ public class HotelReservationServise {
 
     public List<Hotel> getHotels() {
         return hotelList;
+    }
+    
+    public int countDays(String firstDate,String lastDate) {
+        LocalDate startDate = LocalDate.parse(firstDate);
+        LocalDate endDate = LocalDate.parse(lastDate);
+        return  (int) ChronoUnit.DAYS.between(startDate,endDate);
+    }
+    
+    public Hotel findCheapestHotel() {
+    	
+        Hotel cheapestRate  =  hotelList.stream().max(Comparator.comparing(Hotel::getRate)).orElseThrow(NoSuchElementException::new);
+        System.out.println(cheapestRate);
+        return cheapestRate;
     }
 }
