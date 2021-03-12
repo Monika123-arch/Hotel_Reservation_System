f6add584a09f11dca2f77f8c515c131dda0dbb1b
Monika <monikadkale98@gmail.com>
2021-03-12 08:51:03 +0530

    added hotel high rate

--- Hotel.java
+++ Hotel.java
@@ -4,7 +4,17 @@ public class Hotel {
 	
 	private String name;
     private int rate;
+    private int weekdayrate;
+    private int weekendrate;
+    
+	
 
+
+	@Override
+	public String toString() {
+		return "Hotel [name=" + name + ", rate=" + rate + ", weekdayrate=" + weekdayrate + ", weekendrate="
+				+ weekendrate + "]";
+    
     public Hotel(String name, int rate) {
         this.setName(name);
         this.setRate(rate);
@@ -14,8 +24,14 @@ public class Hotel {
 		return rate;
 	}
 
+	@Override
+	public String toString() {
+		return "Hotel [name=" + name + ", rate=" + rate + "]";
+	}
+
 	public void setRate(int rate) {
 		this.rate = rate;
+
 	}
 
 	public String getName() {
@@ -25,4 +41,37 @@ public class Hotel {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public int getWeekdayrate() {
+		return weekdayrate;
+	}
+
+	public void setWeekdayrate(int weekdayrate) {
+		this.weekdayrate = weekdayrate;
+	}
+
+	public int getWeekendrate() {
+		return weekendrate;
+	}
+
+	public void setWeekendrate(int weekendrate) {
+		this.weekendrate = weekendrate;
+	}
+
+	public Hotel(String name, int rate, int weekdayrate, int weekendrate) {
+		super();
+		this.name = name;
+		this.rate = rate;
+		this.weekdayrate = weekdayrate;
+		this.weekendrate = weekendrate;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+   
 }
